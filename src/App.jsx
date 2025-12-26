492585a7dbd4cbb28b80d196170eb1be2853ed84
Rupesh Ranjan <rupesh.ranjan@outlook.in>
2025-12-26 12:05:48 +0530

    added functionality for input of variable grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import TicTacToe from "./components/TicTacToe";
 
 function App() {
+    const [size, setSize] = useState(3);
     return (
         <>
             <h1>Tic Tac Toe</h1>
-            <TicTacToe size={3} />
+            <label className="input-board-size">
+                <div>
+                    Choose Board Size:
+                    <input
+                        type="number"
+                        min="3"
+                        max="10"
+                        value={size}
+                        className="input-board"
+                        onChange={(e) => setSize(parseInt(e.target.value))}
+                    />
+                </div>
+                <p>
+                    Board Size: {size} x {size}
+                </p>
+            </label>
+
+            <TicTacToe size={size} />
         </>
     );
 }
