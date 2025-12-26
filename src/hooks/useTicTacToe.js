99891dc3cf1d39b3d629e4afd7b8f03d85f96646
Rupesh Ranjan <rupesh.ranjan@outlook.in>
2025-12-26 10:13:34 +0530

    made it dynamic for different board size

--- src/hooks/useTicTacToe.js
+++ src/hooks/useTicTacToe.js
@@ -1,33 +1,70 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 const initialBoard = (size) => Array(size * size).fill(null);
 function useTicTacToe(size) {
     const [board, setBoard] = useState(initialBoard(size));
     const [isNextX, setIsNextX] = useState(true);
 
-    const WINNING_PATTERN = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
+    const generateWinningPattern = useMemo(() => {
+        const pattern = [];
+
+        // horizontal and vertical
+        for (let i = 0; i < size; i++) {
+            const horiznotal = [];
+            const vertical = [];
+            for (let j = 0; j < size; j++) {
+                horiznotal.push(i * size + j);
+                vertical.push(i + j * size);
+            }
+            pattern.push(horiznotal, vertical);
+        }
+
+        // diagonal
+        const diagonal1 = [];
+        const diagonal2 = [];
+        for (let i = 0; i < size; i++) {
+            diagonal1.push(i + i * size); // 0,5,10,11
+            diagonal2.push((size - 1) * (i + 1)); // 3, 6, 9, 12
+        }
+        pattern.push(diagonal1, diagonal2);
+        return pattern;
+    }, [size]);
+    // 3, 6, 9, 12
+    //  -> 3 * 1 -> 3
+    // (size - 1) * (i + 1) -> 3 * 2 -> 6
+    // (size - 1) * (i + 1) -> 3 * 3 -> 9
+    // (size - 1) * (i + 1) -> 3 * 4 -> 12
+
+    const WINNING_PATTERN = generateWinningPattern;
+    console.log("Winning Pattern", WINNING_PATTERN);
 
     const calculateWinner = () => {
+        console.log("first");
         for (let i = 0; i < WINNING_PATTERN.length; i++) {
-            const [a, b, c] = WINNING_PATTERN[i];
-            if (board[a] && board[a] === board[b] && board[b] === board[c]) {
-                return board[a];
-            }
+            const pattern = WINNING_PATTERN[i];
+            let countX = 0;
+            let countO = 0;
+            pattern.forEach((index) => {
+                console.log("val", board[index]);
+                if (board[index] === "X") countX++;
+                else if (board[index] === "O") countO++;
+            });
+
+            if (countX === size) return "X";
+            if (countO === size) return "O";
+            console.log("countX", countX, "countO", countO);
+
+            // const [a, b, c] = WINNING_PATTERN[i];
+            // if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+            //     return board[a];
+            // }
         }
         return null;
     };
     const handleClick = (index) => {
         // check winner or alredy clicked
         const winner = calculateWinner();
+        console.log("Winner", winner);
         if (winner || board[index]) return;
 
         // if not winning case
@@ -39,6 +76,8 @@ function useTicTacToe(size) {
     const getStatusMessage = () => {
         const winner = calculateWinner();
         if (winner) return `Player ${winner} wins!`;
+        const isBoardFull = board.every((cell) => cell !== null);
+        if (isBoardFull) return "It's a draw!";
         return `Player ${isNextX ? "X" : "0"} turn`;
     };
     const resetGame = () => {
